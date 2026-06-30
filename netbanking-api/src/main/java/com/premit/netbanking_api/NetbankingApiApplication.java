4039f3d36ae2551213c719e8e485dc3edb4e6c74
premsai7744 <premsai7744@gmail.com>
2026-06-30 21:19:10 +0530

    Initial commit

---
+++ netbanking-api/src/main/java/com/premit/netbanking_api/NetbankingApiApplication.java
@@ -0,0 +1,13 @@
+package com.premit.netbanking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbankingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetbankingApiApplication.class, args);
+	}
+
+}
---
+++ netbanking-api/src/test/java/com/premit/netbanking_api/NetbankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premit.netbanking_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
